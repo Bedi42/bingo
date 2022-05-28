f106e7deb61a3f26025e6317d6c8c264edbb7a59
Bedi42 <krzyschodak@wp.pl>
2022-05-28 21:51:31 +0200

    basic_bingo_board

---
+++ src/Board.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Board extends Component {
+  render() {
+    return (
+      <>
+        <h2 className="name">Krzychu</h2>
+        <div className="board">
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+          <div className="square">pole</div>
+        </div>
+      </>
+    );
+  }
+}
